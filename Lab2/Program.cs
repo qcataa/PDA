f38dca5744c2db255bfe3150298a1fd3f8e0cc06
Catalin <catalin.mcm13@gmail.com>
2018-03-19 16:21:14 +0200

    Lab1 +Lab2

---
+++ Lab2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Collections;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        private static SemaphoreSlim semaphore;
+
+        public static void Main()
+        {
+            semaphore = new SemaphoreSlim(0, 3);
+
+            Console.WriteLine("{0} tasks can enter the semaphore.",semaphore.CurrentCount);
+            Task[] tasks = new Task[7];
+            
+            for (int i = 0; i <= 6; i++)
+            {
+                tasks[i] = Task.Run(() => {
+                    Console.WriteLine("Task {0} begins and waits for the semaphore.",Task.CurrentId);
+                    semaphore.Wait();
+                    Console.WriteLine("Task {0} enters the semaphore.",Task.CurrentId);
+                    Thread.Sleep(1000);
+                    Console.WriteLine("Task {0} releases the semaphore; previous count: {1}.",Task.CurrentId, semaphore.Release());
+                });
+            }
+            
+            Thread.Sleep(500);
+            
+            Console.Write("Main thread calls Release(3) --> ");
+            semaphore.Release(3);
+            Console.WriteLine("{0} tasks can enter the semaphore.",semaphore.CurrentCount);
+
+            Task.WaitAll(tasks);
+
+            Console.WriteLine("Main thread exits.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Mutex/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading;
+using System.Collections;
+
+namespace Lab1
+{
+    class Program
+    {
+        Queue myQ = new Queue();
+
+        Mutex mtx = new Mutex(false);
+
+        int i = 0;
+
+        public void Producer()
+        {
+            while (true)
+            {
+                Thread.Sleep(1000);
+
+                mtx.WaitOne();
+
+                while(myQ.Count == 5)
+                {
+                    Console.WriteLine("Producer Waiting");
+                    mtx.ReleaseMutex();
+
+                    Thread.Sleep(10);
+                    mtx.WaitOne();
+                }
+
+                myQ.Enqueue(i++);
+                mtx.ReleaseMutex();
+            }
+        }
+
+        public void Consumer()
+        {
+            while (true)
+            {
+                mtx.WaitOne();
+
+                while (myQ.Count == 0)
+                {
+                    mtx.ReleaseMutex();
+
+                    Thread.Sleep(10);
+
+                    mtx.WaitOne();
+                }
+
+                Console.WriteLine("(Consumed)\t{0}", myQ.Dequeue());
+
+                mtx.ReleaseMutex();
+
+                Thread.Sleep(2000);
+            }
+        }
+    }
+
+    class Test
+    {
+        static void Main(string[] args)
+        {
+
+            Program program = new Program();
+
+
+            Thread p = new Thread(new ThreadStart(program.Producer));
+            Thread c = new Thread(new ThreadStart(program.Consumer));
+
+            p.Start();
+            c.Start();
+
+        }
+    }
+}
